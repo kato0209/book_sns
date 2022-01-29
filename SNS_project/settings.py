3ad4faa145f32fe356c0f4dd7dbe09e06e3bbafd
Your Name <kato.921002@gmail.com>
2022-01-29 12:50:52 +0900

    heroku-deploy-1.4

--- SNS_project/settings.py
+++ SNS_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
